66305c01597ac8f3729e266282e6446f35efa165
Eugene Park <parkeugene7676@gmail.com>
2024-02-07 14:26:30 +0900

    feat: add tailwindcss build

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ echo 'Running migrations...'
 # python manage.py migrate
 
 echo 'Collecting static files...'
+npx tailwindcss -i ./config/static/src/input.css -o ./config/static/src/output.css
 poetry run python manage.py compress --force
 poetry run python manage.py collectstatic --no-input
 
